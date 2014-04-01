b150e8ef8d9350cd1802300dcb82ca0c43ba4d73
Ben S. Thomas <fearlessleaderthomas@gmail.com>
2014-04-01 19:04:43 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SITS Helper 2
+
+By Ben Thomas
+
+This app is a mostly client side application that is to help people manage the paperwork of playing the board game Saganami Island Tactical Simulator. 
+
+This app uses Rails 4 and Ruby 2. There are no other dependencies to get the app running. 
+
+This app will be hosted on Heroku. It uses Backbone.js for client side app management. 
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SITS2::Application.config.secret_key_base = 'a6de9a63dc4c3285071fafc6c34365084540630bc9532c2aeabbed7abe27a24a6064c34f00d3e25cfacab75da9e4c401bef7c1e97944677305e239346d55fe45'
